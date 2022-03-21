0d024a6169778f27094e5444c8bffbe45c043c5a
bevinart <ebpisarsky@gmail.com>
2022-03-21 18:14:58 +0000

    Tests

--- src/main.c
+++ src/main.c
@@ -1,19 +1,22 @@
 #include "../seaware.h"
 
+// Testing file
+
 int main(int argc, char * argv[]) {
     cfg conf;
     char * anotherTest[255][255];
-    anotherTest[0][0] = "Test";
-    anotherTest[0][1] = "ing";
-
+    anotherTest[0][0] = "Test ";
+    anotherTest[0][1] = " ing";
+    
     // files check
-    if (file_exists("test-file.cfg")==true) {
-        remove("test-file.cfg");
-    }
-    create_file("test-file.cfg");
-    append_file("test-file.cfg", print_config(conf));
+    // if (file_exists("test-file.cfg")==true) {
+    //     remove("test-file.cfg");
+    // }
+    // create_file("test-file.cfg");
+    // append_file("test-file.cfg", print_config(conf));
     printf("%s", read_file("test-file.cfg"));
     printf("%i\n", get_file_size("test-file.cfg"));
+    conf = read_config(read_file("test-file.cfg"), ':'); 
 
     return 0;
 }
