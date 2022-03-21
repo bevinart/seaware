3eca1e9f0780fe6be4382d2db38bace339a91fad
bevinart <ebpisarsky@gmail.com>
2022-03-21 00:53:59 +0000

    Fixed problem with returning remove_char value

--- converter.h
+++ converter.h
@@ -1,6 +1,8 @@
 #ifndef CONVERTER_H
 #define CONVERTER_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 char * remove_char(char * str, char delim);
--- src/converter.c
+++ src/converter.c
@@ -1,7 +1,7 @@
 #include "../converter.h"
 
-char  * remove_char(char * str, char delim) {
-    char newStr[strlen(str)+1];
+char * remove_char(char * str, char delim) {
+    char * newStr = (char*)malloc(strlen(str)+1);
     int ptr = 0;
     for (int i = 0; i < strlen(str); i++) {
         if (str[i]!=delim) {
@@ -9,6 +9,5 @@ char  * remove_char(char * str, char delim) {
             ptr++;
         }
     }
-    newStr[ptr] = '\0';
     return newStr;
 }
