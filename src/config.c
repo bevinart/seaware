b50d4ea01f4ae0ef335a2a49d25e7d39cd0d1c7e
bevinart <ebpisarsky@gmail.com>
2022-03-21 18:14:49 +0000

    singleValues semi-done

--- src/config.c
+++ src/config.c
@@ -13,10 +13,51 @@ cfg read_config(char * cfgFile, char delim) {
     else {
         char * placeholder = read_file(cfgFile);
         config = (char*)malloc(strlen(placeholder)+1);
-        config = cfgFile;
+        config = placeholder;
     }
+
+    config = remove_char(config, ' ');
+    config = remove_comments(config);
+    
+    // Needs value counter for memory allocation
+    newConf.singleValues = (char ***)malloc(sizeof(config)+1);
+    newConf.multiValues = (char ****)malloc(sizeof(config)+1);
+
     for (int i = 0; i < strlen(config); i++) {
-       
+        if () {
+
+        }
+
+        // Multi Value
+        if (config[i]==delim && config[i+1]=='{') {
+
+        }
+        // Single Value
+        else if (config[i]==delim) {
+            char * pholder = config;
+            char * split = strtok(pholder, ":");
+            char * firstValue = split;
+            split = strtok(NULL, ":");
+            char * secondValue = strtok(split, "\n");
+
+            newConf.singleValues[newConf.singleSize] = (char **)malloc(strlen(firstValue)+strlen(secondValue)+1);
+            newConf.singleValues[newConf.singleSize][0] = (char*)malloc(strlen(firstValue)+1);
+            newConf.singleValues[newConf.singleSize][0] = firstValue;
+
+            newConf.singleValues[newConf.singleSize][1] = (char*)malloc(strlen(secondValue)+1);
+            newConf.singleValues[newConf.singleSize][1] = secondValue;
+
+            printf("%s\n", newConf.singleValues[newConf.singleSize][1]);
+
+            newConf.singleSize++;
+        }
+
+        // normal char
+        else {
+            printf("%c", config[i]);
+            continue;
+        }
+        continue;
     }
 
 
