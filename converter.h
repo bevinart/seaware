a31f39d170a3aa2337b6fbaf98d25b14309e9fda
bevinart <ebpisarsky@gmail.com>
2022-03-21 18:14:38 +0000

    Warning to those using printf()

--- converter.h
+++ converter.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+    If using printf(), DO NOT USE %s! It won't print out the value, instead, just call the function as the first argument
+*/
+
 char * remove_char(char * str, char delim);
 
 #endif
