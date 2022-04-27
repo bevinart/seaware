64072543b81e6c483be2789e12f053c06cc216af
bevinart <ebpisarsky@gmail.com>
2022-04-27 18:51:52 +0000

    Tests for string.h added

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 // Testing file
 
 int main(int argc, char * argv[]) {
-    cfg conf;
+    //cfg conf;
     char * anotherTest[255][255];
     anotherTest[0][0] = "Test ";
     anotherTest[0][1] = " ing";
@@ -14,9 +14,16 @@ int main(int argc, char * argv[]) {
     // }
     // create_file("test-file.cfg");
     // append_file("test-file.cfg", print_config(conf));
-    printf("%s", read_file("test-file.cfg"));
-    printf("%i\n", get_file_size("test-file.cfg"));
-    conf = read_config(read_file("test-file.cfg"), ':'); 
+    // printf("%s", read_file("test-file.cfg"));
+    // printf("%i\n", get_file_size("test-file.cfg"));
+    // conf = read_config(read_file("test-file.cfg"), ':');
+
+    printf(strstr("testPoint", "Point"));
+
+    char * as_test = add_strings(3, "Another", "Test", "lamayo");
+    printf("String is: %s\n", as_test);
+    printf("%s\n", itos(12));
+    printf("%s\n", ftos(12.f));
 
     return 0;
 }
