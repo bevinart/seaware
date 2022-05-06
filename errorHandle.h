fef788467cd0b96da311a6c47ee9cf5fe03dd66a
bevinart <ebpisarsky@gmail.com>
2022-05-06 18:12:04 +0000

    Added spacing for err and warn handles

--- errorHandle.h
+++ errorHandle.h
@@ -1,7 +1,7 @@
 #ifndef ERROR_HANDLE_H
 #define ERROR_HANDLE_H
 
-#define ERR(x) printf("fatal error | line %d | file %s\n%s", __LINE__, __FILE__, x)
-#define WARN(x) printf("WARNING | line %d | file %s\n%s", __LINE__, __FILE__, x)
+#define ERR(x) printf("fatal error | line %d | file %s\n%s\n", __LINE__, __FILE__, x)
+#define WARN(x) printf("WARNING | line %d | file %s\n%s\n", __LINE__, __FILE__, x)
 
 #endif
