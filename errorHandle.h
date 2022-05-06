67d471b4eb5890b0c8ebc5708f35170322c4f91c
bevinart <ebpisarsky@gmail.com>
2022-05-06 00:14:09 +0000

    Added professional errorHandler

--- errorHandle.h
+++ errorHandle.h
@@ -1,7 +1,7 @@
 #ifndef ERROR_HANDLE_H
 #define ERROR_HANDLE_H
 
-#define WARNING ""
-#define ERROR ""
+#define ERR(x) printf("fatal error | line %d | file %s\n%s", __LINE__, __FILE__, x)
+#define WARN(x) printf("WARNING | line %d | file %s\n%s", __LINE__, __FILE__, x)
 
 #endif
