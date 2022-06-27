f0cb27d37dc981db765e2b0f995d711bceaa35f9
ThatPrimitive <ebpisarsky@gmail.com>
2022-06-27 09:18:41 -0700

    JSON organization and restart

--- filetypes/json.c
+++ filetypes/json.c
@@ -1,195 +1,142 @@
 #include "json.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
 
-struct JSON parse(char * jsonLocale) {
-    // Allocating variables
-    struct JSON * jsonStruct = malloc(sizeof(struct JSON));
+#define MAX_LINE_SIZE 255
+
+char * removeSpacing(char * code);
+
+// Global Functions
+
+// Creates and allocates JSON variable
+struct JSON * json_init() {
+    struct JSON * jsonStruct = malloc(sizeof(struct JSON)+1);
+    jsonStruct->nextField = malloc(sizeof(struct JSON *)+1);
+    jsonStruct->property = NULL;
+    jsonStruct->values = NULL;
     jsonStruct->prevField = NULL;
-    jsonStruct->VALUE_COUNT = 0;
+    jsonStruct->valuesSize = 0;
     jsonStruct->nextFieldSize = 0;
-    char * json = malloc(strlen(jsonLocale)+1);
 
-    // File Check
-    if (file_exists(jsonLocale))
-        json = realloc(json, get_file_size(jsonLocale)+1);
+    return jsonStruct;
+}
 
-    // If file exists, read it, otherwise, read string
-    strcpy(json, (file_exists(jsonLocale)) ? read_file(jsonLocale) : jsonLocale);
-    strcat(json, "\0");
+// I/O Functions
 
-    // Removes all spacing
-    json = remove_char(json, ' ');
-    json = remove_char(json, '\n');
+// Reads and interprets JSON file
+struct JSON * read_json(struct JSON * json, char * filePath) {
+    // Local loop variables
+    FILE * jsonFile = fopen(filePath, "r");
+    char line[MAX_LINE_SIZE];
 
-    // Local Interpreter variables
-    int BLOCK_COUNT = 0;
-    jsonStruct->nextField = malloc(sizeof(struct JSON *));  // idk where to place this
-    char pHolder[MAX_VALUE_SIZE];
-    int pHolderPtr = 0;
+    while(fgets(line, sizeof(line), jsonFile)) {
+        json = interpret_json(json, line);
+    }
+
+    fclose(jsonFile);
+
+    return json;
+}
+
+// JSON Interpreter
+struct JSON * interpret_json(struct JSON * json, char * jsonCode) {
+    // Local interpreter variables
+    char * newJsonCode = removeSpacing(jsonCode);
 
     // For every character in JSON...
-    for (int i = 0; i < strlen(json); i++) {
+    for (int i = 0; i < strlen(newJsonCode); i++) {
+        // printf("%c", newJsonCode[i]);
+
         // The interpreter .. .... .. 
-        switch (json[i]) {
+        switch (jsonCode[i]) {
 
             // :
             case DELIM: {
-                // If NOT opening a new block
-                if (json[i+1]!='{') {
-                    // Allocate values to include 2 values
-                    jsonStruct->values[jsonStruct->VALUE_COUNT] = malloc(sizeof(char**)*2);
-                    jsonStruct->values[jsonStruct->VALUE_COUNT][0] = malloc(sizeof(char*)*strlen(pHolder));
-                    // Adding element name
-                    pHolder[pHolderPtr] = '\0';
-                    strcpy(jsonStruct->values[jsonStruct->VALUE_COUNT][0], pHolder);
-                    pHolder[0] = '\0';
-                    pHolderPtr = 0;
-                }
+
+                // Gets property name
+
+                // Sets property name
+
                 break;                
             }
 
             // ,
             case SEPARATOR: {
-                // NOT going out of a block
-                if (json[i-1]!='}') {
-                    // Save last value
-                    jsonStruct->values[jsonStruct->VALUE_COUNT][1] = malloc(sizeof(pHolder));
-                    pHolder[pHolderPtr] = '\0';
-                    strcpy(jsonStruct->values[jsonStruct->VALUE_COUNT][1], pHolder);
-
-                    // Increase # of values
-                    jsonStruct->VALUE_COUNT++;
-
-                    // Reset
-                    pHolder[0] = '\0';
-                    pHolderPtr = 0;
-                }
+                
                 break;
             }
 
             // {
             case OPEN_BLOCK_DELIM: {
-                // if not opening of new JSON
-                if (BLOCK_COUNT==0) {
-                    // Set title to NULL
-                    jsonStruct->property = NULL;
-                }
-                // Blocks
-                // "Block": {}
-                else {
-                    // Allocates memory and assigns for next field to enter
-                    struct JSON * nextField = malloc(sizeof(struct JSON));
-                    nextField->nextField = malloc(sizeof(struct JSON *));
-                    nextField->VALUE_COUNT = 0;
-                    nextField->nextFieldSize = 0;
-
-                    // Node machine
-                    nextField->prevField = jsonStruct;
-                    jsonStruct->nextField[jsonStruct->nextFieldSize] = nextField;
-                    jsonStruct = jsonStruct->nextField[jsonStruct->nextFieldSize];
-
-                    // Gets block name
-                    pHolder[pHolderPtr] = '\0';
-                    jsonStruct->property = malloc(sizeof(char*)*strlen(pHolder));
-                    strcpy(jsonStruct->property, pHolder);
-                    pHolder[0] = '\0';
-                    pHolderPtr = 0;
-                }
-
-                // Allocates values
-                jsonStruct->values = malloc(sizeof(char***));
-                // We in one block
-                BLOCK_COUNT++;
                 break;
             }
+
             // }
             case CLOSE_BLOCK_DELIM: {
-                // Gets last value from block
-                if (json[i-1]!='}') {
-                    pHolder[pHolderPtr] = '\0';
-                    jsonStruct->values[jsonStruct->VALUE_COUNT][1] = malloc(sizeof(pHolder));
-                    strcpy(jsonStruct->values[jsonStruct->VALUE_COUNT][1], pHolder);
-                    jsonStruct->VALUE_COUNT++;
-                    pHolder[0] = '\0';
-                    pHolderPtr = 0;
-                }
-
-
-                // To not confuse whats closing a block or closing JSON
-                if (jsonStruct->prevField != NULL) {
-                    jsonStruct = jsonStruct->prevField;
-                }
-
-                // We out of one block
-                jsonStruct->nextFieldSize++;
-                BLOCK_COUNT--;
+                // if (json->prevField != NULL) {
+                //     json = json->prevField;
+                // }
                 break;
             }
 
             // []
             case OPEN_ARRAY_DELIM: {
-                // Starts at 1 since 0 is the element name
-                int arrLen = 1;
+                // Local loop variables
+                char * token = strtok(jsonCode, "[],");
+                char ** values = malloc(sizeof(char*)+1);
+                int iPos = 0;
+                token = strtok(NULL, "[],");
 
-                // Value getter variables
-                char str[255];
-                int strPtr = 0;
+                for (int i = 0; token != NULL; i++) {
+                    i += strlen(token)+1;
 
-                // Skips OPEN_ARRAY_DELIM so that it doesn't interfere with the name or value
-                i++;
-                // Another interpreter for the line
-                // Increases the interpreter value
-                for (; json[i] != CLOSE_ARRAY_DELIM; i++) {
-                    // if : or ] (Split or last value)
-                    if (json[i] == SEPARATOR || json[i+1] == CLOSE_ARRAY_DELIM) {
-                        // Reallocates size to fit more than 2 or whatever the current size is
-                        jsonStruct->values[jsonStruct->VALUE_COUNT] = realloc(jsonStruct->values[jsonStruct->VALUE_COUNT], sizeof(char**) * arrLen+1);
-                        
-                        // Allocates and adds value
-                        jsonStruct->values[jsonStruct->VALUE_COUNT][arrLen] = malloc(sizeof(char*)*strlen(str));
-                        str[strPtr] = '\0';
-                        strcpy(jsonStruct->values[jsonStruct->VALUE_COUNT][arrLen], str);
-                        str[0] = '\0';
-                        strPtr = 0;
-
-                        // Increase array size for adding values
-                        arrLen++;
-                        continue;
-                    }
-                    // Ignore quotes
-                    else if (json[i] == QUOTE) {
-                        continue;
-                    }
-
-                    // Gets values
-                    else {
-                        str[strPtr] = json[i];
-                        strPtr++;
-                        continue;
-                    }
+                    token = strtok(NULL, "[],");
                 }
-                
-            }
-
-            // "" Ignored
-            case QUOTE: {
                 break;
             }
-            
-            // Grabs element name or value
+
+            // Break out of switch-case
+            case ' ':
             default: {
-                pHolder[pHolderPtr] = json[i];
-                pHolderPtr++;
                 break;
             }
         }
-        continue;
     }
+}
 
-    // Freeing variables
-    free(json);
+// Local Functions
 
-    return *jsonStruct;
+// Removes all spaces that aren't inside of QUOTEs
+char * removeSpacing(char * code) {
+    bool isQuoted = false;
+    char * newCode = malloc(strlen(code)+1);
+    int newCodePtr = 0;
+
+    for (int i = 0; i < strlen(code); i++) {
+        switch (code[i])
+        {
+            case QUOTE: {
+                isQuoted = !isQuoted;
+                newCode[newCodePtr] = code[i];
+                newCodePtr++;
+                break;
+            }
+            case ' ': {
+                if (isQuoted==true) {
+                    newCode[newCodePtr] = code[i];
+                    newCodePtr++;
+                }
+                break;
+            }   
+            default:
+                newCode[newCodePtr] = code[i];
+                newCodePtr++;
+                break;
+        }
+        newCode[newCodePtr] = '\0';
+    }
+    printf(newCode);
+    return newCode;
 }
--- filetypes/json.h
+++ filetypes/json.h
@@ -10,53 +10,35 @@
 #define SEPARATOR ','
 #define QUOTE '"'
 
-#define MAX_VALUE_SIZE 255
-
-#include "../fs/files.h"
-#include "../datatypes/string.h"
-
-
 struct JSON {
-    // Field name, if no name, then NULL
+    // Field name, if Global, then NULL
     char * property;
-    // All elements in current field
-    char *** values;
-    int VALUE_COUNT;
 
-    // Field-to-Field
-    /*
-        "BLOCK-1": {
-            ...
-        }
-        "BLOCK-2": {
-            ...
-        }
-    */
+    char *** values;
+    int valuesSize;
+
     struct JSON ** nextField;
     int nextFieldSize;
     struct JSON * prevField;
 };
 
-// Can take a file or a string
-// A.K.A Parses each object (God forbid you use a different JSON file)
-/*
-    {
-        ...
-    }
-    {
-        ...
-    }
-*/
-struct JSON parse(char * jsonLocale);
+// MUST BE CALLED FIRST
+// Initializes a new JSON struct
+struct JSON * json_init();
 
-// With these functions, 'struct JSON json' MUST BE IN FIELD WITH VALUE YOU WANT!!
-// Adds new element in field
-// "Property": "Value"
-struct JSON append_element(struct JSON json, char * propertyName, char * value);
-struct JSON change_element_name(struct JSON json, char * propertyName, char * newName);
-struct JSON change_element_value(struct JSON json, char * propertyName, char * newValue);
-// Removes all elements with that property name
-struct JSON remove_element(struct JSON json, char * propertyName);
-struct JSON output_json(struct JSON json, char * filePath);
+// Parsing
+
+// Allows for single values and arrays
+struct JSON * add_variable(struct JSON * json, char * varName, char ** varValue);
+struct JSON * add_field(struct JSON * json, char * fieldName);
+
+// I/O
+
+// JSON Interpreter
+struct JSON * interpret_json(struct JSON * json, char * jsonCode);
+// Reads and interprets JSON file and return new JSON struct
+struct JSON * read_json(struct JSON * json, char * filePath);
+// Outputs the given JSON struct into a JSON file
+struct JSON * output_json(struct JSON json, char * filePath);
 
 #endif
